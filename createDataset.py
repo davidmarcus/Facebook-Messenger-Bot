cf874a80c9514a6991b1bd667d84ccab42f9bf21
David <davidmarcus@gmail.com>
2018-03-24 03:33:21 -0700

    Load iPhone SMS text

--- createDataset.py
+++ createDataset.py
@@ -8,33 +8,34 @@ personName = raw_input('Enter your full name: ')
 fbData = raw_input('Do you have Facebook data to parse through (y/n)?')
 googleData = raw_input('Do you have Google Hangouts data to parse through (y/n)?')
 linkedInData = raw_input('Do you have LinkedIn data to parse through (y/n)?')
+iosData = raw_input('Do you have ios data to parse through (y/n)?')
 
 def getGoogleHangoutsData():
 	# Putting all the file names in a list
 	allFiles = []
 	# Edit these file and directory names if you have them saved somewhere else
 	for filename in os.listdir('GoogleTextForm'):
-	    if filename.endswith(".txt"): 
+	    if filename.endswith(".txt"):
 	        allFiles.append('GoogleTextForm/' + filename)
 
 	responseDictionary = dict() # The key is the other person's message, and the value is my response
 	# Going through each file, and recording everyone's messages to me, and my responses
 	for currentFile in allFiles:
 		myMessage, otherPersonsMessage, currentSpeaker = "","",""
-		openedFile = open(currentFile, 'r') 
+		openedFile = open(currentFile, 'r')
 		allLines = openedFile.readlines()
 	   	for index,lines in enumerate(allLines):
 	   		# The sender's name is separated by < and >
 	   	    leftBracket = lines.find('<')
 	   	    rightBracket = lines.find('>')
-	        
+
 	        # Find messages that I sent
 	   	    if (lines[leftBracket+1:rightBracket] == personName):
 	   	        if not myMessage:
 	   	            # Want to find the first message that I send (if I send multiple in a row)
 	   	            startMessageIndex = index - 1
 	   	        myMessage += lines[rightBracket+1:]
-	        
+
 	   	    elif myMessage:
 	   	        # Now go and see what message the other person sent by looking at previous messages
 	   	        for counter in range(startMessageIndex, 0, -1):
@@ -58,7 +59,7 @@ def getGoogleHangoutsData():
 
 def getFacebookData():
 	responseDictionary = dict()
-	fbFile = open('fbMessages.txt', 'r') 
+	fbFile = open('fbMessages.txt', 'r')
 	allLines = fbFile.readlines()
 	myMessage, otherPersonsMessage, currentSpeaker = "","",""
 	for index,lines in enumerate(allLines):
@@ -71,7 +72,7 @@ def getFacebookData():
 	            # Want to find the first message that I send (if I send multiple in a row)
 	            startMessageIndex = index - 1
 	        myMessage += justMessage[colon+2:]
-	        
+
 	    elif myMessage:
 	        # Now go and see what message the other person sent by looking at previous messages
 	        for counter in range(startMessageIndex, 0, -1):
@@ -89,7 +90,7 @@ def getFacebookData():
 	                responseDictionary[otherPersonsMessage] = myMessage
 	                break
 	            otherPersonsMessage = justMessage[colon+2:] + otherPersonsMessage
-	        myMessage, otherPersonsMessage, currentSpeaker = "","",""    
+	        myMessage, otherPersonsMessage, currentSpeaker = "","",""
 	return responseDictionary
 
 def getLinkedInData():
@@ -124,6 +125,68 @@ def getLinkedInData():
 	            myMessage = myMessage + str(row['Content']) + " "
 	return responseDictionary
 
+def getIosData():
+	import pandas as pd
+	import sqlite3
+	con = sqlite3.connect('3d0d7e5fb2ce288813306e4d4636395e047a3d28')
+	messages = pd.read_sql_query('''
+		-- more info http://aaron-hoffman.blogspot.com/2017/02/iphone-text-message-sqlite-sql-query.html
+		select
+		 m.rowid
+		,coalesce(m.cache_roomnames, h.id) ThreadId
+		,m.is_from_me IsFromMe
+		,case when m.is_from_me = 1 then m.account
+		 else h.id end as FromPhoneNumber
+		,case when m.is_from_me = 0 then m.account
+		 else coalesce(h2.id, h.id) end as ToPhoneNumber
+		,m.service Service
+
+		/*,datetime(m.date + 978307200, 'unixepoch', 'localtime') as TextDate -- date stored as ticks since 2001-01-01 */
+		,datetime((m.date / 1000000000) + 978307200, 'unixepoch', 'localtime') as TextDate /* after iOS11 date needs to be / 1000000000 */
+
+		,m.text MessageText
+
+		,c.display_name RoomName
+
+		from
+		message as m
+		left join handle as h on m.handle_id = h.rowid
+		left join chat as c on m.cache_roomnames = c.room_name /* note: chat.room_name is not unique, this may cause one-to-many join */
+		left join chat_handle_join as ch on c.rowid = ch.chat_id
+		left join handle as h2 on ch.handle_id = h2.rowid
+
+		where
+		-- try to eliminate duplicates due to non-unique message.cache_roomnames/chat.room_name
+		(h2.service is null or m.service = h2.service)
+
+		order by
+		 2 -- ThreadId
+		,m.date
+	''', con)
+	messages = messages[messages['MessageText'].notnull()]
+	messages['MessageText'] = messages['MessageText'].apply(lambda x: x.encode('ascii', errors='ignore')).str.strip()
+	messages = messages[messages['MessageText'] != '']
+	responseDictionary = {}
+	for _, grp in messages.groupby('ThreadId'):
+		if len(grp) <= 1:
+			continue
+		current_from_me = otherPersonsMessage = None
+		for _, row in grp.iterrows():
+			if current_from_me is not None and row['FromPhoneNumber'] == current_from_phone_number:
+				# continue message
+				message_parts.append(row['MessageText'])
+			else:
+				if current_from_me == 1 and otherPersonsMessage is not None:
+					myMessage = '. '.join(message_parts).replace('..', '.')
+					responseDictionary[otherPersonsMessage] = myMessage
+				elif current_from_me == 0:
+					otherPersonsMessage = '. '.join(message_parts).replace('..', '.')
+				current_from_me = row['IsFromMe']
+				current_from_phone_number = row['FromPhoneNumber']
+				message_parts = [row['MessageText']]
+	return responseDictionary
+
+
 def cleanMessage(message):
 	# Remove new lines within message
 	cleanedMessage = message.replace('\n',' ').lower()
@@ -145,6 +208,9 @@ if (fbData == 'y'):
 if (linkedInData == 'y'):
 	print 'Getting LinkedIn Data'
 	combinedDictionary.update(getLinkedInData())
+if (iosData == 'y'):
+	print 'Getting IOS Data'
+	combinedDictionary.update(getIosData())
 print 'Total len of dictionary', len(combinedDictionary)
 
 print 'Saving conversation data dictionary'
